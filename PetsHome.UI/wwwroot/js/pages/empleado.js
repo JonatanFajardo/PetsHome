30616d5e0cfc5bca73c70aa20af77b709cc3c325
JonatanFajardo <69583436+JonatanFajardo@users.noreply.github.com>
2022-02-25 10:22:57 -0600

    Codigo mejor estructurado en el medoto AddAsync en controladores.

--- PetsHome.UI/wwwroot/js/pages/empleado.js
+++ PetsHome.UI/wwwroot/js/pages/empleado.js
@@ -8,9 +8,9 @@ var Empleado = (function () {
             var header = new Array();
             //Nombre | Tama�o/AutoWidth | Visibilidad
             header = [
-                { FieldName: 'emp_Id', Visibility: false  },
+                { FieldName: 'emp_Id', Visibility: false },
                 { FieldName: 'emp_Codigo', Size: 200, Visibility: true },
-                { FieldName: 'emp_Nombre', Visibility: true  },
+                { FieldName: 'emp_Nombres', Visibility: true },
                 { FieldName: 'cag_Descripcion', Visibility: true },
                 { FieldName: 'refg_Nombre', Visibility: true },
                 { FieldName: 'emp_EsActivo', Visibility: true }
--- PetsHome.UI/wwwroot/js/pages/historial-medico.js
+++ PetsHome.UI/wwwroot/js/pages/historial-medico.js
@@ -8,7 +8,12 @@ var HistorialMedico = (function () {
             var header = new Array();
             //Nombre | Tama�o/AutoWidth | Visibilidad
             header = [
-
+                { FieldName: 'medic_Id', Visibility: false },
+                { FieldName: 'masc_Id', Size: 200, Visibility: true },
+                { FieldName: 'medic_Esterilizacion', Visibility: true },
+                { FieldName: 'com_Id', Visibility: true },
+                { FieldName: 'medic_SaludCuidado', Visibility: true },
+                { FieldName: 'medic_InformacionAdicional', Visibility: true }
             ];
             datatable.init(Direction, header);
         })
