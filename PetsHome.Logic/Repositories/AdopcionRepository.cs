f764a0a83a6d3e92eec903be68c30cc63d5fa55c
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2025-10-23 20:29:22 -0600

    feat(adopciones): agregar vista de detalle y flujo de selección de adoptante

--- PetsHome.Logic/Repositories/AdopcionRepository.cs
+++ PetsHome.Logic/Repositories/AdopcionRepository.cs
@@ -33,13 +33,22 @@ namespace PetsHome.Logic.Repositories
             return await DbApp.Detail<PR_Refugio_Adopciones_DetailResult>(sqlQuery, parameter);
         }
 
+        // Devuelve todas las solicitudes (solicitantes) para una mascota específica
+        public async Task<PR_Refugio_Adopciones_DetailResult> DetailByMascotaAsync(int masc_Id)
+        {
+            const string sqlQuery = "[Refugio].[PR_Refugio_Adopciones_Detail]";
+            var parameter = new DynamicParameters();
+            parameter.Add("@masc_Id", masc_Id, DbType.Int32, ParameterDirection.Input);
+            return await DbApp.Detail<PR_Refugio_Adopciones_DetailResult>(sqlQuery, parameter);
+        }
+
         public async Task<Boolean> AddAsync(tbAdopciones entity)
         {
             entity.adop_UsuarioCrea = 1;
-            const string sqlQuery = "[Refugio].[PR_Refugio_Adopcion_Insert]";
+            const string sqlQuery = "[Refugio].[PR_Refugio_Adopciones_Insert]";
             var parameter = new DynamicParameters();
             parameter.Add("@sol_Id", entity.sol_Id, DbType.Int32, ParameterDirection.Input);
-            parameter.Add("@adop_Estado", entity.adop_Estado, DbType.Boolean, ParameterDirection.Input);
+            parameter.Add("@adop_Estado", entity.adop_Estado, DbType.String, ParameterDirection.Input);
             parameter.Add("@adop_UsuarioCrea", entity.adop_UsuarioCrea, DbType.Int32, ParameterDirection.Input);
             return await DbApp.Insert(sqlQuery, parameter);
         }
@@ -51,7 +60,7 @@ namespace PetsHome.Logic.Repositories
             var parameter = new DynamicParameters();
             parameter.Add("@adop_Id", entity.adop_Id, DbType.Int32, ParameterDirection.Input);
             parameter.Add("@sol_Id", entity.sol_Id, DbType.Int32, ParameterDirection.Input);
-            parameter.Add("@adop_Estado", entity.adop_Estado, DbType.Boolean, ParameterDirection.Input);
+            parameter.Add("@adop_Estado", entity.adop_Estado, DbType.String, ParameterDirection.Input);
             parameter.Add("@adop_UsuarioModifica", entity.adop_UsuarioModifica, DbType.Int32, ParameterDirection.Input);
             return await DbApp.Update(sqlQuery, parameter);
         }
@@ -62,6 +71,15 @@ namespace PetsHome.Logic.Repositories
             var parameter = new DynamicParameters();
             parameter.Add("@depto_Id", id, DbType.Int32, ParameterDirection.Input);
             return await DbApp.Delete(sqlQuery, parameter);
+        }
+
+        // Solicitudes por mascota desde el SP de adopciones detail (para tabs en Adopción)
+        public async Task<PR_Refugio_Adopciones_DetailResult> ListByMascotaFromAdopcionesDetailAsync(int masc_Id)
+        {
+            const string sqlQuery = "[Refugio].[PR_Refugio_Adopciones_Detail]";
+            var parameter = new DynamicParameters();
+            parameter.Add("@masc_Id", masc_Id, DbType.Int32, ParameterDirection.Input);
+            return await DbApp.Select<PR_Refugio_Adopciones_DetailResult>(sqlQuery, parameter);
         }
     }
 }
