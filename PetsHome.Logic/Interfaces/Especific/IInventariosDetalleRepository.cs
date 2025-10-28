e0923f20236b12d5b474e9612da7dae649e4d2dd
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2025-10-27 21:17:23 -0600

    chore(inventario): eliminar módulo de inventario y actualizar desde origen

--- PetsHome.Logic/Interfaces/Especific/IInventariosDetalleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetsHome.Logic.Interfaces.Especific
-{
-    /// <summary>
-    /// Interfaz que define las operaciones de la entidad InventariosDetalle.
-    /// </summary>
-    internal interface IInventariosDetalleRepository
-    {
-    }
-}
