0b61070bae2b7c39ace0eb489e3747d43e9c0cd4
Jonatan Fajardo <jonatanfajardo000@gmail.com>
2026-04-25 22:52:33 -0600

    style(ui): actualizar estilos de home, tablas y texto de datatables

--- PetsHome.UI/wwwroot/js/main.js
+++ PetsHome.UI/wwwroot/js/main.js
@@ -162,8 +162,8 @@ var appConfig = (function () {
     //            zeroRecords: "No se encontraron resultados",
     //            emptyTable: "Ningún dato disponible en esta tabla",
     //            info:
-    //                "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-    //            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+    //                "Mostrando _START_ de _TOTAL_ registros",
+    //            infoEmpty: "Mostrando 0 al 0 de un total de 0 registros",
     //            infoFiltered: "(filtrado de un total de _MAX_ registros)",
     //            infoPostFix: "",
     //            search: "",
